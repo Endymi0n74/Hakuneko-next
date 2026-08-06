db11bf6aa95ec15080a00c6c690c6c768c20c1a1
Endymion <stephane.rivoire74@gmail.com>
2026-08-06 23:31:26 +0200

    chore: remove backup file and fix invalid import

--- web/src/frontend/fluent-core/FrontendFluentCore_e2e.ts
+++ web/src/frontend/fluent-core/FrontendFluentCore_e2e.ts
@@ -1,4 +1,4 @@
-import { describe, beforeEach, it/*, expect*/ } from 'vitest';
+import { describe, beforeEach, it/*, expect } from 'vitest';
 import { TestFixture } from './FrontendFluentCore_fixture';
 
 describe.sequential('Front-End (Fluent-Core)', { timeout: 60_000 }, () => {
